cf2b7b91b644d37ed16d76f136ca4c883f921e15
nafilah17 <rissanafilah@gmail.com>
2020-07-13 18:38:03 +0700

    lesson 8 when where (not complete)

--- app/Dogs.php
+++ app/Dogs.php
@@ -3,17 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Carbon;
-
 
 class Dogs extends Model
 {
-	protected $fillable = ['name'];
-
-	function setNameAttribute($value){
-		return $this->attributes['name'] = strtoupper($value);
-	}
-
-
+    //
 }
